0eb12552f4747a5c531a6b0b2a74d4c0646184e5
Timthom <monsson47thomas@gmail.com>
2023-04-21 17:48:16 +0200

    Adding more views to AnimalDetailView

---
+++ Africa/View/InsetFactView.swift
@@ -0,0 +1,36 @@
+//
+//  InsetFactView.swift
+//  Africa
+//
+//  Created by Thomas Månsson on 21/04/2023.
+//
+
+import SwiftUI
+
+struct InsetFactView: View {
+    // MARK: - PROPERTYS
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        GroupBox {
+            TabView {
+                ForEach(animal.fact, id: \.self) { item in
+                    Text(item)
+                }
+            }//: TAB
+            .tabViewStyle(PageTabViewStyle())
+            .frame(minHeight: 148, idealHeight: 168, maxHeight: 180)
+        }//; GROUPBOX
+    }
+}
+// MARK: - PREVIEW
+struct InsetFactView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        InsetFactView(animal: animals[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
